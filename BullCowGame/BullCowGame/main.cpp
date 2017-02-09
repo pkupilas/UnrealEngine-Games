d37aceac4b65f0ecaaf6dfd11e19ccfb09104c1f
pkupilas <pm.kupilas@gmail.com>
2017-02-09 20:16:49 +0100

    [BullCowGame] UI started.

--- BullCowGame/BullCowGame/main.cpp
+++ BullCowGame/BullCowGame/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
 
-	std::cout << "Hello World!.\n";
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bulls and Cows game.\n";
+	cout << "Can You guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+
+	cout << "Enter Your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	cout << "Your guess: " << Guess << endl;
 
 	return 0;
 }
