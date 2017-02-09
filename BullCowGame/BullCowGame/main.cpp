df027714cb92e83eb46b595abd626be8cd98a57b
pkupilas <pm.kupilas@gmail.com>
2017-02-09 22:28:27 +0100

    [BullCowGame] Added guessing.

--- BullCowGame/BullCowGame/main.cpp
+++ BullCowGame/BullCowGame/main.cpp
@@ -3,18 +3,38 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
 int main() {
 
-	constexpr int WORD_LENGTH = 5;
+	int NUMBER_OF_TURNS = 5;
 
-	cout << "Welcome to Bulls and Cows game.\n";
-	cout << "Can You guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	PrintIntro();
 
-	cout << "Enter Your guess: ";
-	string Guess = "";
-	cin >> Guess;
-
-	cout << "Your guess: " << Guess << endl;
+	for (int i=0;i<NUMBER_OF_TURNS;i++)
+	{
+		GetGuessAndPrintBack();
+		cout << endl;
+	}
 
 	return 0;
+}
+
+void PrintIntro()
+{
+	constexpr int WORD_LENGTH = 5;
+	
+	cout << "Welcome to Bulls and Cows game.\n";
+	cout << "Can You guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+}
+
+string GetGuessAndPrintBack()
+{
+	string guess = "";
+	cout << "Enter Your guess: ";
+	getline(cin, guess);
+	cout << "Your guess: " << guess << endl;
+
+	return guess;
 }
