75b9921e72fed2274dd3786e314533bdeab7c3c2
Piotr Kupilas <pkupilas@11bits.com>
2019-12-05 12:52:01 +0100

    [GAS] Added GE_Melee_Damage and printing current health on console.

--- GAS/Source/GAS/Private/AttributeSetBase.cpp
+++ GAS/Source/GAS/Private/AttributeSetBase.cpp
@@ -1,9 +1,20 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "AttributeSetBase.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
 
 UAttributeSetBase::UAttributeSetBase()
 {
 	Health = 200.0f;
+}
+
+void UAttributeSetBase::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData &Data)
+{
+	UProperty* changedProperty = Data.EvaluatedData.Attribute.GetUProperty();
+	UProperty* healthProperty = FindFieldChecked<UProperty>(UAttributeSetBase::StaticClass(), GET_MEMBER_NAME_CHECKED(UAttributeSetBase, Health));
+
+	if (changedProperty == healthProperty)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%f"), Health.GetCurrentValue());
+	}
 }
