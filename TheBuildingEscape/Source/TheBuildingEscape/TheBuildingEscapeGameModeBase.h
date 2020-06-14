bdd573c039d5813c5c3ae9c385f9c23c8c03955c
Piotr <pm.kupilas@gmail.com>
2020-06-14 17:04:31 +0200

    [TheBuildingEscape] Added new project.

---
+++ TheBuildingEscape/Source/TheBuildingEscape/TheBuildingEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheBuildingEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEBUILDINGESCAPE_API ATheBuildingEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
